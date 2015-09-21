c2b907de1cc335fcec18947d48b50fceb4a14828
hmlingesh <Lingesh.M@happiestminds.com>
2015-09-21 16:09:57 +0530

    Update jacoco_agent.rb

--- lib/java_buildpack/framework/jacoco_agent.rb
+++ lib/java_buildpack/framework/jacoco_agent.rb
@@ -36,8 +36,8 @@ module JavaBuildpack
       def release
       #-javaagent:D:\jacoco\lib\jacocoagent.jar=address=%JACOCO_SERVER_URL%,port=%JACOCO_SERVER_PORT%,output=tcpclient,includes=com.covisint.platform.clog.*,append=true"
        java_opts   = @droplet.java_opts
-       server_url ='localhost' #default url
-       server_port = '6300' #default port
+       #server_url ='localhost' #default url
+       #server_port = '6300' #default port
        
        if ENV.has_key?('JACOCO_SERVER_URL')
           unless ENV['JACOCO_SERVER_URL'].nil? && ENV['JACOCO_SERVER_URL'].empty?
@@ -51,7 +51,7 @@ module JavaBuildpack
           end
        end
        
-       configuration = "lib/jacocoagent.jar=output=tcpclient,address="+server_url+",port="+server_port+",includes=*,append=true"
+       #configuration = "lib/jacocoagent.jar=output=tcpclient,address="+server_url+",port="+server_port+",includes=*,append=true"
        puts configuration
        java_opts.add_javaagent(@droplet.sandbox + "lib/jacocoagent.jar=output=tcpclient,address=localhost,port=6300,includes=*,append=true")
        #@droplet.java_opts
